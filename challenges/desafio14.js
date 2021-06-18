d2a73a0f0978d3810d7f82cc1aeae5a924cd25af
Vagner Pereira <sousa.vagner82@gmail.com>
2021-06-18 14:52:10 -0300

    desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,25 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: { $subtract: ["$stopTime", "$startTime"] },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: { $divide: ["$duracaoMedia", 1000 * 60] } },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
